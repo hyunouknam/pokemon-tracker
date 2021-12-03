775e58b2994a20f52a6d6ebae92a02d0d0590096
Hyun Ouk Nam <hyunouk.nam@gmail.com>
2021-12-03 14:44:55 -0500

    Started a Spring Boot Application

---
+++ src/main/java/com/example/pokemontracker/PokemonTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.pokemontracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pokemontracker/PokemonTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pokemontracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
