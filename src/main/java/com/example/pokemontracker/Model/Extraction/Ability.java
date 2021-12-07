12099f751ea9431f33b73bbd557d8e343f95a394
Hyun Ouk Nam <hyunouk.nam@gmail.com>
2021-12-06 23:38:59 -0500

    Abilities are properly received from JSON

--- src/main/java/com/example/pokemontracker/Model/Extraction/Ability.java
+++ src/main/java/com/example/pokemontracker/Model/Extraction/Ability.java
@@ -1,13 +1,34 @@
 package com.example.pokemontracker.Model.Extraction;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonKey;
 
-import java.util.List;
+import java.util.Map;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Ability {
 
+    String is_hidden;
+    @JsonIgnoreProperties(value = "url")
+    Map<String,String> ability;
+
     public Ability() {
 
+    }
+
+    public String getIs_hidden() {
+        return is_hidden;
+    }
+
+    public void setIs_hidden(String is_hidden) {
+        this.is_hidden = is_hidden;
+    }
+
+    public Map<String, String> getAbility() {
+        return ability;
+    }
+
+    public void setAbility(Map<String, String> ability) {
+        this.ability = ability;
     }
 }
